7cc7765122e444a95e12737d8e4309252584fa35
blavikensbutcher <luckertheory@gmail.com>
2024-07-05 18:18:59 +0300

    remove js files

--- src/auth/decorators/auth.decorator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Auth = void 0;
-var common_1 = require("@nestjs/common");
-var jwt_auth_guard_1 = require("../guards/jwt-auth.guard");
-var Auth = function () { return (0, common_1.UseGuards)(jwt_auth_guard_1.JwtAuthGuard); };
-exports.Auth = Auth;
