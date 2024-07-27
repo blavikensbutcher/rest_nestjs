0356037d60ec3aaee308b1301ee7ad209e1eb4a9
blavikensbutcher <luckertheory@gmail.com>
2024-07-27 19:22:50 +0300

    remove domain

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -78,7 +78,7 @@ export class AuthService {
 
     res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
       httpOnly: true,
-      domain: 'localhost',
+      // domain: 'localhost',
       expires: expiresIn,
       secure: true,
       // lax in prod
@@ -89,7 +89,7 @@ export class AuthService {
   removeRefreshToken(res: Response) {
     res.cookie(this.REFRESH_TOKEN_NAME, '', {
       httpOnly: true,
-      domain: 'localhost',
+      // domain: 'localhost',
       expires: new Date(0),
       secure: true,
       // lax in prod
